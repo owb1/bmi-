2b91f4955aea9150d7f7bfa720f16a124cd0e5ad
owb1 <160222965+owb1@users.noreply.github.com>
2024-02-16 17:49:21 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -20,16 +20,25 @@ def calculate_bmi():
     bmi = weight/(height)**2
     bmi_thresholds = [18.5, 23, 27.5]
     level_labels = ['Risk of nutritional deficiency','Low Risk','Moderate Risk','High Risk']
-    if bmi <= bmi_thresholds[0]:
-        level = level_labels[0]
-    elif bmi <= bmi_thresholds[1]:
-        level = level_labels[1]
-    elif bmi <= bmi_thresholds[2]:
-        level = level_labels[2]
+    if height > 250:
+        return 'what are you? A giraffe?'
+      elif weight > 200:
+        return 'one at a time please'
+      elif height < 100:
+        return 'haha midget'
+      elif weight < 5:
+        return 'what animal are you? A cat?'
     else:
-        level = level_labels[3]
-    st. success(f"Your BMI is {bmi}. You are at {level}")
-
-button = st.button("Calculate BMI")
-if button:
-    calculate_bmi()
+        if bmi <= bmi_thresholds[0]:
+            level = level_labels[0]
+        elif bmi <= bmi_thresholds[1]:
+            level = level_labels[1]
+        elif bmi <= bmi_thresholds[2]:
+            level = level_labels[2]
+        else:
+            level = level_labels[3]
+        st. success(f"Your BMI is {bmi}. You are at {level}")
+    
+    button = st.button("Calculate BMI")
+    if button:
+        calculate_bmi()
